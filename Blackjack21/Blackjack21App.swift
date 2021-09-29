d4b5fac47ad4a5a2d57b8a0c2e0fdfacd1dae229
Tim Randall <trtimrandall@gmail.com>
2021-09-29 22:52:02 +1000

    Initial Commit

---
+++ Blackjack21/Blackjack21App.swift
@@ -0,0 +1,17 @@
+//
+//  Blackjack21App.swift
+//  Blackjack21
+//
+//  Created by Tim Randall on 29/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Blackjack21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blackjack21/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blackjack21
+//
+//  Created by Tim Randall on 29/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
