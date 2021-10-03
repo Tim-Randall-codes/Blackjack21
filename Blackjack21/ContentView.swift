1ebbac983f336e8c99d1a2547da1d5341d8cf738
Tim Randall <trtimrandall@gmail.com>
2021-10-04 00:29:15 +1100

    ui of page 1 done

--- Blackjack21/ContentView.swift
+++ Blackjack21/ContentView.swift
@@ -14,12 +14,15 @@ struct ContentView: View {
         ZStack{
             Background()
             VStack{
-            
                 Title(words: "Blackjack 21")
+                Spacer()
+                Image("AS").resizable()
+                    .frame(width: 100, height: 150)
                 Spacer()
                 TextWidget(words: "You have $\(String(userMoney.num)).")
                 TextWidget(words: "How much would you like to bet?")
-                TextField("Enter bet here", text: $betString)
+                TextField("Enter your bet here", text: $betString).fixedSize()
+                    .frame(width: 200, height: 50)
                 Button(action:{
                     viewRouter.currentPage = 2
                 }, label:{
